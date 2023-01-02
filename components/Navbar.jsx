10781642ae4f0e384ed79bba7a1851a41db007eb
Erik Ivan Retana Lopez <merzelert@gmail.com>
2023-01-02 13:13:11 -0600

    portfolio nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,17 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+import React, { useState } from 'react'
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai'
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
     return (
         <div className='fixed w-full h-20 shadow-xl z-[100]'>
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
@@ -26,18 +34,23 @@ const Navbar = () => {
                             <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
                         </Link>
                     </ul>
-                    <div className='md:hidden'>
+                    <div onClick={handleNav} className='md:hidden cursor-pointer p-3'>
                         <AiOutlineMenu size={25} />
                     </div>
                 </div>
             </div>
 
-            <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
-                <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+            <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
+                <div className={
+                    nav 
+                    ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500' 
+                    : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
+                    }
+                >
                     <div>
                         <div className='flex w-full items-center justify-between'>
                             <Image src='/../public/assets/1280px-ER_logo.svg.png' width='87' height='35' alt='/' />
-                            <div className='rounded-full shadow-lg shadow-gr p-3 cursor-pointer'>
+                            <div onClick={handleNav} className='rounded-full shadow-lg shadow-gr p-3 cursor-pointer'>
                                 <AiOutlineClose />
                             </div>
                         </div>
@@ -65,6 +78,20 @@ const Navbar = () => {
                         </ul>
                         <div className='pt-40'>
                             <p className='uppercase tracking-widest text-[#5651e5]'>Let`s Connect</p>
+                            <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+                                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                    <FaLinkedinIn />
+                                </div>
+                                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                    <FaGithub />
+                                </div>
+                                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                    <AiOutlineMail />
+                                </div>
+                                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                    <BsFillPersonLinesFill />
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
