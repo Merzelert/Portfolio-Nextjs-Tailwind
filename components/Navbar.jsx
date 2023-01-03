db5e1f13167082f0ced5a8b32fdc59452b527c05
Erik Ivan Retana Lopez <merzelert@gmail.com>
2023-01-03 12:28:10 -0600

    portfolio image src

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react'
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai'
 import { FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
+import {logoImg} from '/../public/assets/1280px-ER_logo.svg.png'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
@@ -28,7 +29,7 @@ const Navbar = () => {
         <div className={shadow ? 'fixed w-full h-20 shadow-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
             <div className='flex justify-between items-center w-full h-full px-4 2xl:px-16 bg-[#ecf0f3]'>
                 <Link href='/#home' scroll={false}>
-                    <Image src='/../public/assets/1280px-ER_logo.svg.png' alt='/' width={125} height={50} />
+                    <Image src={logoImg} alt='/' width={125} height={50} />
                 </Link>
                 <div>
                     <ul className='hidden md:flex'>
