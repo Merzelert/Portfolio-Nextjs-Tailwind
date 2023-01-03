05f257ab72750532aa357fecbd380122b064010e
Erik Ivan Retana Lopez <merzelert@gmail.com>
2023-01-02 19:36:19 -0600

    portfolio scroll

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -8,7 +8,7 @@ import { HiOutlineChevronDoubleUp } from 'react-icons/hi'
 
 const Contact = () => {
     return (
-        <div className='w-full lg:h-screen'>
+        <div id='contact' className='w-full lg:h-screen'>
             <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
                 <p className='text-3xl tracking-widest uppercase text-[#5651e5]'>Contact</p>
                 <h2 className='py-4'>Get In Touch</h2>
@@ -76,7 +76,7 @@ const Contact = () => {
                     </div>
                 </div>
                 <div className='flex justify-center py-12'>
-                    <Link href='/'>
+                    <Link href='#home' scroll={false}>
                         <div className='rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                             <HiOutlineChevronDoubleUp className='text-[#5651e5]' size={30}/>
                         </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,36 +1,50 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai'
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
+    const [shadow, setShadow] = useState(false)
 
     const handleNav = () => {
         setNav(!nav)
     }
 
+    useEffect(() => {
+        const handleShadow = () => {
+            if (window.scrollY >= 90) {
+                setShadow(true)
+            } else {
+                setShadow(false)
+            }
+        };
+        window.addEventListener('scroll', handleShadow)
+    }, []);
+
     return (
-        <div className='fixed w-full h-20 shadow-xl z-[100]'>
+        <div className={shadow ? 'fixed w-full h-20 shadow-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-                <Image src='/../public/assets/1280px-ER_logo.svg.png' alt='/' width='125' height='50' />
+                <Link href='#home' scroll={false}>
+                    <Image src='/../public/assets/1280px-ER_logo.svg.png' alt='/' width='125' height='50' />
+                </Link>
                 <div>
                     <ul className='hidden md:flex'>
-                        <Link href='/'>
+                        <Link href='#home' scroll={false}>
                             <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
                         </Link>
-                        <Link href='/'>
+                        <Link href='#about' scroll={false}>
                             <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
                         </Link>
-                        <Link href='/'>
+                        <Link href='#skills' scroll={false}>
                             <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
                         </Link>
-                        <Link href='/'>
+                        <Link href='#projects' scroll={false}>
                             <li className='ml-10 text-sm uppercase hover:border-b'>Projects</li>
                         </Link>
-                        <Link href='/'>
+                        <Link href='#contact' scroll={false}>
                             <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
                         </Link>
                     </ul>
@@ -42,10 +56,10 @@ const Navbar = () => {
 
             <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
                 <div className={
-                    nav 
-                    ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500' 
-                    : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
-                    }
+                    nav
+                        ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
+                        : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
+                }
                 >
                     <div>
                         <div className='flex w-full items-center justify-between'>
@@ -60,19 +74,19 @@ const Navbar = () => {
                     </div>
                     <div className='py-4 flex flex-col'>
                         <ul className='uppercase'>
-                            <Link href='/'>
+                            <Link href='#home'>
                                 <li className='py-4 text-sm'>Home</li>
                             </Link>
-                            <Link href='/'>
+                            <Link href='#about'>
                                 <li className='py-4 text-sm'>About</li>
                             </Link>
-                            <Link href='/'>
+                            <Link href='#skills'>
                                 <li className='py-4 text-sm'>Skills</li>
                             </Link>
-                            <Link href='/'>
-                                <li className='py-4 text-sm'>Project</li>
+                            <Link href='#projects'>
+                                <li className='py-4 text-sm'>Projects</li>
                             </Link>
-                            <Link href='/'>
+                            <Link href='#contact'>
                                 <li className='py-4 text-sm'>Contact</li>
                             </Link>
                         </ul>
