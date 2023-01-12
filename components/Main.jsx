d6613162fd3e16fc9b4a6b44dc1760e9e0110411
Erik Ivan Retana Lopez <merzelert@gmail.com>
2023-01-11 23:00:44 -0600

    bg logo

--- components/Main.jsx
+++ components/Main.jsx
@@ -17,17 +17,17 @@ const Main = () => {
                     </h1>
                     <p className='py-4 text-gray-200 max-w-[70%] m-auto'>I&apos;m a Full-Stack Web Developer with the ability to actively participate in work teams, organized, proactive, with excellent performance when learning new technologies like Next.js, Python for backend and Observability with New Relic and always willing to face any problem.</p>
                     <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
-                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <div className='rounded-full bg-[#5651e5] shadow-md shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                             <a href="https://www.linkedin.com/in/erik-retana-webdev/" target="_blank" rel="noopener noreferrer"><FaLinkedinIn /></a>
                         </div>
-                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <div className='rounded-full bg-[#5651e5] shadow-md shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                             <a href="https://github.com/Merzelert" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
                         </div>
-                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <div className='rounded-full bg-[#5651e5] shadow-md shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                             <a onClick={() => window.location = 'mailto:erik.retana.lopez@gmail.com'} target="_blank"><AiOutlineMail /></a>
 
                         </div>
-                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <div className='rounded-full bg-[#5651e5] shadow-md shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                             <a href="https://twitter.com/merzelert" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
 
                         </div>
